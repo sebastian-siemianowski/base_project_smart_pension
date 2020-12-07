33ad27ba399a12e6e8350d0d95aef13b87cbf6dd
sebastiansiemianowski <rudph2@gmail.com>
2020-12-07 10:54:48 +0000

    Modifies Web Log Repository

--- lib/repositories/web_log_entry_repository.rb
+++ lib/repositories/web_log_entry_repository.rb
@@ -1,13 +1,39 @@
 class WebLogEntryRepository
-  attr_reader :all
+  attr_reader :all, :cache_expired
 
   def initialize
     @all = []
+    @cache_valid = true
   end
 
   def add(web_log_entry)
     return unless web_log_entry.is_a?(WebLogEntry)
+    
+    if web_log_entry.valid?
+      @all << web_log_entry
+      @cache_valid = false
+    end
+  end
 
-    @all << web_log_entry if web_log_entry.valid?
+  def most_popular_web_pages
+    if @most_popular_web_pages && @cache_valid
+      return @most_popular_web_pages
+    else
+      @most_popular_web_pages = sorted_log_entries
+    end
+  end
+
+  private
+
+  def sorted_log_entries
+    log_entries_with_website_visits.sort { |a, b| b[1] <=> a[1] }
+  end
+
+  def log_entries_with_website_visits
+    grouped_log_entries.map { |web_address, ip_addresses| [web_address, ip_addresses.count] }
+  end
+
+  def grouped_log_entries
+    @all.group_by { |web_log_entry| web_log_entry.page_address }
   end
 end
