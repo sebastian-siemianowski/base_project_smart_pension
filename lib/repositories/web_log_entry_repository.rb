fe101a5c1d13a69d1bdb5685736c9ac51d75314a
sebastiansiemianowski <rudph2@gmail.com>
2020-12-07 11:10:10 +0000

    Adds helper methods for extracting most popular pages

--- lib/repositories/web_log_entry_repository.rb
+++ lib/repositories/web_log_entry_repository.rb
@@ -17,7 +17,17 @@ class WebLogEntryRepository
 
   def most_popular_web_pages
     if @most_popular_web_pages && @cache_valid
-      return @most_popular_web_pages
+      @most_popular_web_pages
+    else
+      @most_popular_web_pages = sorted_log_entries
+      @cache_valid = true
+      @most_popular_web_pages
+    end
+  end
+  
+  def most_popular_unique_pages
+    if @most_popular_web_pages && @cache_valid
+      @most_popular_web_pages
     else
       @most_popular_web_pages = sorted_log_entries
       @cache_valid = true
@@ -27,15 +37,22 @@ class WebLogEntryRepository
 
   private
 
-  def sorted_log_entries
-    log_entries_with_website_visits.sort { |a, b| b[1] <=> a[1] }
+  def sorted_log_entries(unique: false)
+    log_entries_with_website_visits(unique: unique).sort { |a, b| b[1] <=> a[1] }
   end
 
-  def log_entries_with_website_visits
-    grouped_log_entries.map { |web_address, ip_addresses| [web_address, ip_addresses.count] }
+  def log_entries_with_website_visits(unique: false)
+    if unique
+      grouped_log_entries.map do |web_address, ip_addresses|
+        unique_addresses = ip_addresses.map(&:ip_address).uniq
+        [web_address, unique_addresses.count]
+      end
+    else
+      grouped_log_entries.map { |web_address, ip_addresses| [web_address, ip_addresses.count] }
+    end
   end
 
   def grouped_log_entries
-    @all.group_by { |web_log_entry| web_log_entry.page_address }
+    @all.group_by(&:page_address)
   end
 end
